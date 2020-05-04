929ff1106f2174e1a959c21d40fb3ee43b73d089
Alexei Udarbe <alexeiudarbe@AU-MacBook-Bro.local>
2020-05-03 20:41:38 -0700

    updated game logic and styling (need to fix win logic!)

--- js/main.js
+++ js/main.js
@@ -17,51 +17,60 @@ let winner;
 /*----- cached element references -----*/
 let cellEls = Array.from(document.querySelectorAll('#gameBoard > div'));
 let msgEl = document.getElementById('msg');
-console.log(msgEl)
+let resetButton = document.getElementById('reset-button');
+
 /*----- event listeners -----*/
 document.getElementById('gameBoard').addEventListener('click', handleClick);
-
+resetButton.addEventListener('click', init);
 /*----- functions -----*/
 init();
 
 function init() {
     board =
     [
-        null, 1, null, //top
-        null, null, -1, //mid
+        null, null, null, //top
+        null, null, null, //mid
         null, null, null //bottom
     ];
-
     turn = 1 //set this to random later
-    winner = null; //1, -1, 't', null
-	renderBoard();
-	renderMessage();
+    winner = null;
+	render();
 };
 
-function renderBoard() {
-    board.forEach(function(cellVal, cellIdx) { //look up arguments for forEach. This was a blocker.
+function render() {
+	board.forEach(function(cellVal, cellIdx) { //look up params for forEach. This was a blocker.
 		cellEls[cellIdx].style.backgroundColor = players[cellVal];
-		console.log(cellIdx + '>' + cellVal)
     });
-}
-
-function renderMessage() {
 	if (winner) {
-		if (winner === 't') {
-			msgEl.innerHTML = `Cat's Game!`;
-		} else {
-			msgEl.innerHTML = `<span style="color: ${players[turn]}">${players[turn].toUpperCase()}</span> wins!`;	
-		}
+		msgEl.innerHTML = `<span style="color: ${players[turn]}">${players[turn].toUpperCase()}</span> wins!`;	
+	} else if (winner === 't') {
+		msgEl.innerHTML = `Cat's Game!`;
 	} else {
-		msgEl.innerHTML = `<span style="color: ${players[turn]}">${players[turn].toUpperCase()}'s</span> turn!`;
+		msgEl.innerHTML = `<span style="color: ${players[turn]}">${players[turn].toUpperCase()}'s</span> turn!`;	
 	}
 }
 
-function handleClick() {
-	console.log('click')
+function handleClick(event) {
+	let currentCellIdx = cellEls.indexOf(event.target);
+	if (board[currentCellIdx] || winner !== null) return;
+	board[currentCellIdx] = turn;
+	turn *= -1;
+	winner = getWinner();
+	render();
 }
 
-
+function getWinner() {
+		for (let i = 0; i < winCombo.length; i++) {
+			if (Math.abs(board[winCombo[i][0]] + board[winCombo[i][1]] + board[winCombo[i][2]]) === 3) {
+				return board[winCombo[i][0]];
+			};
+		};
+		if (board.includes(null)) {
+			return null;
+		} else {
+			return 't';
+		}
+	};
 
 
 /*----- pseudocode -----
@@ -89,23 +98,23 @@ function handleClick() {
 			-->	4.3.1.1.3) Set the background color of the current element by using the value as a key on the colors lookup object (constant).
 		4.2.2) Render a message:
 			--> 4.2.2.1) If winner has a value other than null (game still in progress), render whose turn it is - use the color name for the player, converting it to upper case.
-			-- >4.2.2.2) If winner is equal to 'T' (tie), render a tie message.
+			--> 4.2.2.2) If winner is equal to 'T' (tie), render a tie message.
 			--> 4.2.2.3) Otherwise, render a congratulatory message to which player has won - use the color name for the player, converting it to uppercase.
 	--> 4.3) Wait for the user to click a square
 
 5) Handle a player clicking a square:
-	5.1) Obtain the index of the square that was clicked by either:
-		5.1.1) "Extracting" the index from an id assigned to the element in the HTML, or
-		5.1.2) Looping through the cached square elements using a for loop and breaking out when the current square element equals the event object's target.
-	5.2) If the board has a value at the index, immediately return because that square is already taken.
-	5.3) If winner is not null, immediately return because the game is over.
-	5.4) Update the board array at the index with the value of turn.
-	5.5) Flip turns by multiplying turn by -1 (flips a 1 to -1, and vice-versa).
+	--> 5.1) Obtain the index of the square that was clicked by either:
+		--> 5.1.1) "Extracting" the index from an id assigned to the element in the HTML, or
+		*** 5.1.2) Looping through the cached square elements using a for loop and breaking out when the current square element equals the event object's target.
+	--> 5.2) If the board has a value at the index, immediately return because that square is already taken.
+	--> 5.3) If winner is not null, immediately return because the game is over.
+	--> 5.4) Update the board array at the index with the value of turn.
+	--> 5.5) Flip turns by multiplying turn by -1 (flips a 1 to -1, and vice-versa).
 	5.6) Set the winner variable if there's a winner:
-		5.6.1) Loop through the each of the winning combination arrays defined.
-		5.6.2) Total up the three board positions using the three indexes in the current combo.
-		5.6.3) Convert the total to an absolute value (convert any negative total to positive).
-		5.6.4) If the total equals 3, we have a winner! Set winner to the board's value at the index specified by the first index in the combo array. Exit the loop.
+		--> 5.6.1) Loop through the each of the winning combination arrays defined.
+		??> 5.6.2) Total up the three board positions using the three indexes in the current combo.
+		??> 5.6.3) Convert the total to an absolute value (convert any negative total to positive).
+		??> 5.6.4) If the total equals 3, we have a winner! Set winner to the board's value at the index specified by the first index in the combo array. Exit the loop.
 	5.7) If there's no winner, check if there's a tie:
 		5.7.1) Set winner to 'T' if there are no more nulls in the board array.
 	5.8) All state has been updated, so render the state to the page (step 4.2).
